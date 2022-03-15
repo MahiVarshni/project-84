b8f5e0038307a8b1f8560f0f911e69af43549931
MahiVarshni <84715191+MahiVarshni@users.noreply.github.com>
2022-03-15 13:05:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAedVu6Ge9gMVsfQV1pooS0ZunRrcgYqqk",
+  authDomain: "spectagram-project-1bdc6.firebaseapp.com",
+  projectId: "spectagram-project-1bdc6",
+  storageBucket: "spectagram-project-1bdc6.appspot.com",
+  messagingSenderId: "309852706454",
+  appId: "1:309852706454:web:4ae9bfda66e4f01f9c94b7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
